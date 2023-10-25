e9ce0aa3df702843dd48526eaac1116932f2b155
taka01150810 <taka01150810@gmail.com>
2023-10-25 21:03:10 +0900

    コードを変更してみよう

---
+++ app/src/components/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="main-page">
+      メインページです
+    </div>
+</template>
+  
+  <script>
+  export default {
+  }
+  </script>
+  
+  <style scoped>
+  .main-page {
+    padding: 10px;
+  }
+  </style>
+  
