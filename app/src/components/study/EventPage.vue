838348f268fa409ef76f01c56d0006217b3009fc
taka01150810 <taka01150810@gmail.com>
2023-11-04 16:10:01 +0900

    イベント処理

--- app/src/components/study/EventPage.vue
+++ app/src/components/study/EventPage.vue
@@ -1,11 +1,49 @@
 <template>
   <div class="event-page">
     <h2>学習3：イベント</h2>
+
+    <!-- 従来のJavaScript -->
+    <h3><span class="badge badge-secondary">JavaScript</span></h3>
+    <span id="countText">クリックされた回数は0回です </span>
+    <button id="button" class="btn btn-sm btn-info">Click Me</button>
+    <br />
+    <br />
+
+    <!-- Vue.js -->
+    <h3><span class="badge badge-secondary">Vue.js</span></h3>
+    <span>クリックされた回数は{{ count }}回です </span>
+    <!-- v-on:click = @click 対象の要素をクリックした際に指定した関数を呼び出す -->
+    <button id="button" class="btn btn-sm btn-info" @click="onClickButton()">
+      Click Me
+    </button>
   </div>
 </template>
 
 <script>
-export default {};
+// 従来のJavaScript
+let count = 0;
+window.onload = function () {
+  const button = document.getElementById("button");
+  button.onclick = function () {
+    count++;
+    const countText = document.getElementById("countText");
+    countText.innerHTML = "クリックされた回数は" + count + "回です ";
+  };
+};
+
+export default {
+  data() {
+    return {
+      // 変数を定義
+      count: 0,
+    };
+  },
+  methods: {
+    onClickButton: function () {
+      this.count++;
+    },
+  },
+};
 </script>
 
 <style scoped>
