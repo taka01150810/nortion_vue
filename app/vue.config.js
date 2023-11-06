11e8e35e4fa37b0b87ddd351ad0740f2ff996214
taka01150810 <taka01150810@gmail.com>
2023-11-06 21:19:50 +0900

    タイトルとアイコンを変更しよう

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "TechpitNotion",
+    },
+  },
   devServer: {
     port: 8081,
     disableHostCheck: true,
-    host: 'localhost'
-  }
+    host: "localhost",
+  },
 };
-  
